9d61c20885853f0e02086ad48c306d95184055fd
ss <ss@mail.com>
2018-01-03 16:47:10 +0800

    设置路由及相关组件的引用

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
+import Nav from './components/nav'
+import Article from './components/article'
+import ArticleList from './components/articleList'
 import App from './App'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router: new VueRouter({
+    mode: 'history',
+    routes: [
+      { path: '/', name: 'index', component: Nav },
+      { path: '/articleList', name: 'articleList', component: ArticleList },
+      { path: '/article/*', name: 'article', component: Article }
+    ]
+  })
 })
