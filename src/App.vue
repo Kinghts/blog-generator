172bb35a0642e2d474021dbe757a7300babc68a4
hh <hh@email.com>
2018-01-08 22:05:04 +0800

    首页样式调整，添加wrapper

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,45 @@
 <template>
   <div id="app">
     <background></background>
+    <wrapper class="wrapper"></wrapper>
+    <navigator class="nav"></navigator>
     <router-view class="view"></router-view>
   </div>
 </template>
 
 <script>
 import background from './components/background'
+import nav from './components/nav'
+import wrapper from './components/wrapper'
 export default {
   name: 'app',
   components: {
-    background: background
+    background: background,
+    navigator: nav,
+    wrapper: wrapper
   }
 }
 </script>
 
-<style>
+<style lang="less">
   @import './assets/main.css';
   #app {
     width: 100%;
     height: 100%;
     display: flex;
     justify-content: center;
-    align-items: center;
-  }
-  .view {
-    position: relative;
-    z-index: 10;
+    .wrapper {
+      height: 200px;
+      width: 100%;
+    }
+    .nav {
+      position: fixed;
+      top: 0;
+      left: 0;
+    }
+    .view {
+      position: relative;
+      z-index: 10;
+    }
   }
 </style>
