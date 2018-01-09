bbf628d18e7c7a6d5d4304174bf497d078dd856c
ss <ss@mail.com>
2018-01-09 17:03:10 +0800

    article组件样式调整

--- src/components/article.vue
+++ src/components/article.vue
@@ -1,28 +1,44 @@
 <template>
-  <div class="article">
-    <div v-html="article.content" class="content">
+  <article class="article">
+    <header class="article-header">
+      <h1 class="article-title">
+        <router-link :to="{ path: article.routePath }">{{ article.name }}</router-link>
+      </h1>
+      <div class="article-info">
+        <span class="createAt">创建于 {{ article.createAt }}</span>
+        <!--span class="updateAt">{{ article.updateAt }}</span-->
+      </div>
+    </header>
+    <div class="article-body">
+      <div v-html="article.content" class="content"></div>
     </div>
-    <div class="article-info">
-      <span class="createAt">创建于 {{ article.createAt }}</span>
-      <span class="updateAt">{{ article.updateAt }}</span>
-    </div>
-  </div>
+    <footer class="article-footer">
+      <router-link v-if="article.routePath" :to="{ path: article.routePath }">阅读全文 »</router-link>
+    </footer>
+  </article>
 </template>
 
 <script>
 import axios from 'axios'
 export default {
+  props: ['name', 'articleObj'],
   data () {
     return {
       article: {
         name: '',
         createAt: '',
         updateAt: '',
-        content: ''
+        content: '',
+        routePath: ''
       }
     }
   },
   mounted () {
+    if (this.$props.articleObj) {
+      this.$data.article = this.$props.articleObj
+      this.$data.article.name = this.$props.name
+      return
+    }
     axios.get(this.$route.path + '.json')
       .then(res => {
         this.$data.article = res.data
@@ -34,17 +50,48 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
   .article {
     display: flex;
     align-items: center;
     flex-direction: column;
-    flex: 1;
+    width: 772px;
     margin: 20px;
-    padding: 10px;
+    padding: 40px;
     border-radius: 5px;
-    max-width: 1200px;
-    background-color: #555;
     color: #bbb;
+    background-color: #FFF;
+    .article-header {
+      .article-title {
+        text-align: center;
+        font-size: 25px;
+        color: #444;
+        font-weight: 700;
+        a {
+          color: #444;
+          text-decoration: none;
+        }
+      }
+      .article-info {
+        font-size: 12px;
+        color: #555;
+      }
+    }
+    .article-body {
+      margin: 10px 0;
+    }
+    .article-footer {
+      a {
+        border-radius: 5px;
+        padding: 5px 15px;
+        color: #FFF;
+        font-size: 13px;
+        background-color: #97dffd;
+        text-decoration: none;
+        &:hover {
+          background-color: rgba(0, 149, 255, 0.2);
+        }
+      }
+    }
   }
 </style>
