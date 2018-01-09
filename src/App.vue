2986fe478a427df056652e7b67d3e57fdd9e84eb
ss <ss@mail.com>
2018-01-09 17:03:58 +0800

    首页样式调整

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="app">
-    <background></background>
-    <wrapper class="wrapper"></wrapper>
-    <navigator class="nav"></navigator>
-    <router-view class="view"></router-view>
+    <!--background></background-->
+    <header class="header">
+      <navigator class="nav"></navigator>
+      <wrapper class="wrapper"></wrapper>
+    </header>
+    <main class="main">
+      <router-view class="view"></router-view>
+    </main>
+    <footer class="footer">
+      <div class="footer-image"></div>
+      <div class="footer-inner">
+        本博客参考<a href="https://diygod.me">diygod.me</a>的设计，在此表示感谢.
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -22,24 +32,51 @@ export default {
 </script>
 
 <style lang="less">
-  @import './assets/main.css';
+  @import './assets/main.less';
   #app {
     width: 100%;
-    height: 100%;
+    min-height: 100%;
     display: flex;
-    justify-content: center;
-    .wrapper {
-      height: 200px;
-      width: 100%;
-    }
-    .nav {
-      position: fixed;
-      top: 0;
-      left: 0;
-    }
-    .view {
+    flex-direction: column;
+    align-items: center;
+    .header {
       position: relative;
-      z-index: 10;
+      width: 100%;
+      .nav {
+      }
+      .wrapper {
+        height: 400px;
+        width: 100%;
+      }
+    }
+    .main {
+      position: relative;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+    }
+    .footer {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      .footer-image {
+        width: 100%;
+        height: 400px;
+        background-image: url(./assets/img/footer-image.jpg);
+        background-size: cover;
+        background-position: center 50%;
+        background-repeat: no-repeat;
+      }
+      .footer-inner {
+        padding: 10px;
+        color: #888;
+        background-color: #232323;
+        text-align: center;
+        a {
+          color: #795548;
+        }
+      }
     }
   }
 </style>
