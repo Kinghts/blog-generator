d30c661b16a7835ec6f5365b0d56fdf0af2f9dc0
ss <ss@mail.com>
2018-01-30 16:54:44 +0800

    添加分页组件

--- src/components/articleList.vue
+++ src/components/articleList.vue
@@ -2,6 +2,11 @@
   <section class="article-list">
     <article-cell :name="key" :articleObj="article" class="article" :key="key" v-for="(article, key) in articles">
     </article-cell>
+    <pagination
+      @toPage="toPage"
+      :start="1" :total-size="5" :page-size="3" :current="1"
+      class="article-pagination">
+    </pagination>
   </section>
 </template>
 
@@ -9,6 +14,7 @@
 import axios from 'axios'
 import { mapPath } from '../config'
 import article from './article'
+import pagination from './pagination'
 export default {
   data () {
     return {
@@ -38,15 +44,20 @@ export default {
         console.log(err)
       })
   },
+  methods: {
+    toPage (num) {
+      console.log('to page: ' + num)
+    }
+  },
   components: {
-    'article-cell': article
+    'article-cell': article,
+    'pagination': pagination
   }
 }
 </script>
 
 <style scoped>
   .article-list {
-    border-radius: 5px;
     display: flex;
     flex-direction: column;
   }
---
+++ src/components/pagination.vue
@@ -0,0 +1,101 @@
+<template>
+    <nav class="pagination">
+      <span @click="goToPrePage" :class="['pre-btn', { hide: hide==='pre' }]">&lt</span>
+      <span @click="go(n)" v-for="n in range" :key="n" :class="['page-btn', { selected: n===currentPage }]">
+        {{ n }}
+      </span>
+      <span @click="goToNextPage" :class="['next-btn', { hide: hide==='next' }]">&gt</span>
+    </nav>
+</template>
+
+<script>
+export default {
+  props: ['start', 'totalSize', 'pageSize', 'current'],
+  data () {
+    return {
+      currentPage: 0,
+      range: []
+    }
+  },
+  mounted () {
+    let totalSize = parseInt(this.$props.totalSize)
+    let pageSize = parseInt(this.$props.pageSize)
+    this.$data.currentPage = this.$props.current ? parseInt(this.$props.current) : this.$props.start
+    let start, range, index = totalSize - pageSize + 1
+    if (this.$data.currentPage > index) { // 以currentPage为起点时超出范围
+      start = index
+    } else {
+      start = this.$data.currentPage
+    }
+    this.$data.range.push(...Array(pageSize).fill('n').map((v, i) => {return i + start}))
+  },
+  computed: {
+    hide: function () {
+      if (parseInt(this.$props.start) === this.$data.currentPage) {
+        return 'pre'
+      }
+      if (parseInt(this.$props.start) + parseInt(this.$props.totalSize) - 1 === this.$data.currentPage){
+        return 'next'
+      }
+      return ''
+    }
+  },
+  methods: {
+    goToPrePage () {
+      let current = this.$data.currentPage--
+      let range = this.$data.range
+      if (current === this.$data.range[0] && current !== this.$props.start) {
+        range.pop()
+        range.unshift(current - 1)
+      }
+      this.$emit('toPage', current - 1)
+    },
+    go (page) {
+      this.$data.currentPage = page
+      this.$emit('toPage', page)
+    },
+    goToNextPage () {
+      let _props = this.$props, _data = this.$data
+      let current = _data.currentPage++
+      let range = _data.range
+      if (current === _data.range[range.length - 1] && current !== _props.totalSize + _props.start - 1) {
+        range.shift()
+        range.push(current + 1)
+      }
+      this.$emit('toPage', current + 1)
+    },
+    inRange (totalSize, curent) {
+      return 1 <= current && current <= totalSize
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+  .pagination {
+    max-width: 100%;
+    margin: 20px;
+    padding: 20px;
+    border-radius: 5px;
+    background-color: #FFF;
+    text-align: center;
+    span.pre-btn, span.page-btn, span.next-btn {
+      margin: 10px;
+      padding: 5px 10px;
+    }
+    span.page-btn{
+      border: 1px solid #97dffd;
+      border-radius: 5px;
+    }
+    span.page-btn:hover {
+      background-color: #97dffd;
+    }
+    span:hover {
+      cursor: pointer;
+    }
+    span.selected {
+      background-color: #97dffd;
+    }
+  }
+</style>
+
